a2bb7f5189578b46dc2732b4bcd16f00374704c0
Brandon Torres <116675188+Soccermay@users.noreply.github.com>
2024-03-02 21:51:50 -0600

    added JS file

---
+++ script.js
@@ -0,0 +1,44 @@
+function getMilk(bottles, money) {   
+    console.log("leaveHouse");
+    console.log("moveRight");
+    console.log("moveRight");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveRight");
+    console.log("moveRight");
+   
+   
+    console.log("buy " + bottles + " bottles of Milk, which will cost you $" + TotalBottles(money, 1.5) + ".");   
+  
+   
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("enterHouse");
+  
+   return calcChange(money, 1.5);
+  }
+  
+  function TotalBottles(startingPrice, costPerBottle) {
+      var numberofbottles = Math.floor(startingPrice / costPerBottle);
+  
+     return numberofbottles;
+  }
+  
+  function calcChange(startAmount, costPerBottle) {
+    var change = startAmount % costPerBottle;
+  
+    return change;
+   
+  }
+  
+  console.log("Hello there Sir, here is your " + getMilk(12, 64) + " change."); //calling back the function with values
+  
+  
